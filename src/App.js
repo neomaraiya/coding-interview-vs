143e097cf48fdd56c66f5adc965c5f25d2b5a981
neomaraiya <neomaraiya@outlook.com>
2021-05-14 18:42:53 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './style.css';
 
-// Create a custom http hook
-// Use the custom hook to fetch data using the below API -
-// https://reqres.in/api/users?page=1
-// 
-// Create a button and fetch next page data
+// Problem Statement
+// 1. Create a custom http hook
+// 2. Use the custom hook to fetch data using the API (https://reqres.in/api/users?page=1)
+// 3. Display the data
+// 4. Create a button and fetch next page data
 
 export default function App() {
   return (
